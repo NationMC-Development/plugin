44ebefe871b8ea1eeb3ecff7b04bc084472b2fc3
mumeinosato <mumeinosato@gmail.com>
2022-04-05 11:15:32 +0900

    test

--- src/main/java/lm/cmnoitan/nationmc/NationMC.java
+++ src/main/java/lm/cmnoitan/nationmc/NationMC.java
@@ -26,51 +26,13 @@ public final class NationMC extends JavaPlugin {
         getLogger().info("プラグインが停止しました");
         // Plugin shutdown logic
     }
-}
 
-public class MessageDao {
-    //接続に必要な変数を宣言
-    private static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-    private static final String JDBC_URL  = "jdbc:oracle:thin:@localhost:1521:ORCL";
-    private static final String USER_ID   = "imuser";
-    private static final String USER_PASS = "impass";
 
-    MessageDao (){
-        // JDBCドライバのロード
-        try {
-            Class.forName(DRIVER_NAME);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public ArrayList<MessageDto> select() {
-    ArrayList<MessageDto> list = new ArrayList<>();
-
-        // コネクションクラスの宣言
-        Connection con = null;
-        // ステートメントクラスの宣言
-        PreparedStatement ps = null;
-        // リザルトセットクラスの宣言
-        ResultSet rs = null;
-
-        // データベースにアクセス
-        try {
-            // データベースとの接続の確立を行う。
-            con = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
-
-　  }
-    }
-}
 
 public class  BeakEventClass implements Listener{
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-        if
     }
-        int event.getPlayer().getName();
-    //}
-    //public void  onBlockBreakEvent(BlockBreakEvent event){
-
-    //}
-//}
+    public void  onBlockBreakEvent(BlockBreakEvent event){
+    }
+}
