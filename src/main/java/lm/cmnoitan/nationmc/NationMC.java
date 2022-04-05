4be494e613a365a13ebe378391a421221edefbde
mumeinosato <mumeinosato@gmail.com>
2022-04-05 11:53:00 +0900

    cinfig.yml

--- src/main/java/lm/cmnoitan/nationmc/NationMC.java
+++ src/main/java/lm/cmnoitan/nationmc/NationMC.java
@@ -5,6 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,6 +20,12 @@ public final class NationMC extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+        File configYml = new File(getPlugin().getDataFolder(), "config.yml");
+        if (!configYml.exists()){
+            getPlugin().saveResource(configYml.getName(), false);
+        }
         getLogger().info("プラグインが開始しました");
         // Plugin startup logic
 
@@ -27,14 +37,14 @@ public final class NationMC extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+}
 
-
-public class  BeakEventClass implements Listener{
+public class  BeakEventClass implements Listener {
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
-        if (!(event.getPlayer().getName())())
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if ((event.getPlayer().getName()) =!())
     }
-    }
-    public void  onBlockBreakEvent(BlockBreakEvent event){
+
+    public void onBlockBreakEvent(BlockBreakEvent event) {
     }
 }
